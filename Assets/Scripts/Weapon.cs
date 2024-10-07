f19a2cd61924481ba52f7ea3cd43883c6dc740d6
Samuel Salazar <samuels@Mac.lan>
2024-10-06 22:36:53 -0400

    prototipo incial

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity=30;
+    public float bulletPrefabLifeTime=3f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon(){
+        GameObject bullet=Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay){
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
